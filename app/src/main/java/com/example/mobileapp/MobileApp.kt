a910c09f9a487aaff84e4fc3d434d9e9cc0e4aed
Nebul4s <j.hiltula24@gmail.com>
2025-04-20 16:42:00 +0300

    fixed merging issue

--- app/src/main/java/com/example/mobileapp/MobileApp.kt
+++ app/src/main/java/com/example/mobileapp/MobileApp.kt
@@ -1,5 +1,6 @@
 package com.example.mobileapp
 
+import android.util.Log
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
--- app/src/main/java/com/example/mobileapp/ui/screens/LoginViewModel.kt
+++ app/src/main/java/com/example/mobileapp/ui/screens/LoginViewModel.kt
@@ -1,14 +1,32 @@
 package com.example.mobileapp.ui.screens
 
+import android.content.Context
+import android.content.Intent
+import android.content.IntentSender
 import androidx.lifecycle.ViewModel
+import com.example.mobileapp.R
+import com.example.mobileapp.model.Account
+import com.example.mobileapp.model.GoogleAccount
+import com.example.mobileapp.model.UserData
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.BeginSignInRequest.GoogleIdTokenRequestOptions
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.tasks.await
+import kotlin.coroutines.cancellation.CancellationException
 
-class LoginViewModel : ViewModel() {
+class LoginViewModel(
+    private val context: Context,
+    private val oneTapClient: SignInClient
+) : ViewModel() {
     private val firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
     private val _authState = MutableStateFlow<AuthState>(AuthState.Idle)
     val authState: StateFlow<AuthState> = _authState.asStateFlow()
@@ -58,6 +76,83 @@ class LoginViewModel : ViewModel() {
                     _authState.value = AuthState.Error(task.exception?.message ?: "Something went wrong")
                 }
             }
+    }
+
+    //google sign-in
+
+    private val auth = Firebase.auth
+
+    suspend fun signIn(): IntentSender? {
+        val result = try {
+            oneTapClient.beginSignIn(buildSignInRequest()).await()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            if (e is CancellationException) throw e
+            null
+        }
+        return result?.pendingIntent?.intentSender
+    }
+
+    suspend fun signInWithIntent(intent: Intent): UserData? {
+        val credential = oneTapClient.getSignInCredentialFromIntent(intent)
+        val googleIdToken = credential.googleIdToken
+        val googleCredentials = GoogleAuthProvider.getCredential(googleIdToken, null)
+
+        return try {
+            val user = auth.signInWithCredential(googleCredentials).await().user
+            user?.let {
+                val userData = hashMapOf(
+                    "username" to it.displayName,
+                    "email" to it.email,
+                    "uid" to it.uid,
+                )
+
+                val firestore = FirebaseFirestore.getInstance()
+                firestore.collection("users")
+                    .document(it.uid)
+                    .set(userData)
+                    .addOnSuccessListener {
+                        _authState.value = AuthState.Success(user)
+                    }
+                    .addOnFailureListener { exception ->
+                        _authState.value = AuthState.Error(exception.message ?: "Something went wrong")
+                    }
+
+                UserData(
+                    userId = it.uid,
+                    username = it.displayName
+                )
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            if (e is CancellationException) throw e
+            _authState.value = AuthState.Error(e.message ?: "Something went wrong")
+            null
+        }
+    }
+
+    suspend fun signOut() {
+        try {
+            oneTapClient.signOut().await()
+            auth.signOut()
+            _authState.value = AuthState.Idle
+        } catch (e: Exception) {
+            e.printStackTrace()
+            if (e is CancellationException) throw e
+        }
+    }
+
+    private fun buildSignInRequest(): BeginSignInRequest {
+        return BeginSignInRequest.Builder()
+            .setGoogleIdTokenRequestOptions(
+                GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    .setFilterByAuthorizedAccounts(false)
+                    .setServerClientId(context.getString(R.string.web_client_id))
+                    .build()
+            )
+            .setAutoSelectEnabled(true)
+            .build()
     }
 }
 
